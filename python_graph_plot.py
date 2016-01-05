8342f11c6e9ac2c5eae2bd9bd9eccf5805c2f3e5
arttp2 <mail.asquared@gmail.com>
2016-01-05 19:23:11 +0530

    graph plotting utility

---
+++ python_graph_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#use np for mathematical functions
+#use plt for graphing utilities
+
+
+x = [0.880,.84,.785,.685,.61,.533,.500]
+y = [1400,1440,1480,1600,1720,1840,1960]
+plt.plot(x, y)
+plt.show()
